ef2b3cdef955f50e68b4f6a80cba17d4a785b0b9
ignas <iignas61@gmail.com>
2022-05-22 16:31:07 +0300

    v1.5

--- Klases/circle.h
+++ Klases/circle.h
@@ -27,9 +27,9 @@ using std::left;
 
 
 const int C = 50;
-const string duom = "100000rez.txt";
-const string rez = "vargsai100000.txt";
-const string rez1 = "kieti100000.txt";
+const string duom = "1000rez.txt";
+const string rez = "vargsai1000.txt";
+const string rez1 = "kieti1000.txt";
 
 //struct data {
 //    string vardas, pavarde;
@@ -38,6 +38,97 @@ const string rez1 = "kieti100000.txt";
 //    double rez = 0, mediana = 0;
 //
 //};
+
+
+//class zmogus
+//{
+//protected:
+//    string vardas, pavarde;
+//public:
+//    inline string vardass() const {return vardas;}
+//    inline string pavardee() const {return pavarde;}
+//    virtual void setVardas(string v) = 0;
+//    virtual void setPavarde(string p) = 0;
+//    zmogus(string var = "", string pav = "") : vardas{var}, pavarde{pav}{}
+//};
+//
+//
+//class data : public zmogus {
+//    private:
+////	    string vardas, pavarde;
+//	    int egz;
+//	    vector<int> nd;
+//	    double rez, mediana;
+//    public:
+//        data(vector<int> nd = {0}, int egz = 0, double rez = 0, double mediana = 0, string  vardas = "", string pavarde = "") : zmogus{vardas, pavarde}  { }
+//
+//        string getVardas() const { return vardas; }
+//        string getPavarde() const { return pavarde; }
+//        int getEgzaminas() const { return egz; }
+//        const vector<int>& getNd() const { return nd; }
+//        vector<int>& getNd() { return nd; }
+//        double getRez() const { return rez; }
+//        double getMediana() const { return mediana; }
+//
+//        void setVardas(string vardas) { this->vardas = vardas; }
+//        void setPavarde(string pavarde) { this->pavarde = pavarde; }
+//        void setEgzaminas(int egz) { this->egz = egz; }
+//        void setNd(vector<int> nd) { this->nd = nd; }
+//        void setRez(double rez) { this->rez = rez; }
+//        void setMediana(double mediana) { this->mediana = mediana; }
+//
+//        data(const data& that)
+//        {
+//            this->vardas = that.vardas;
+//            this->pavarde = that.pavarde;
+//            this->egz = that.egz;
+//            this->nd = that.nd;
+//            this->rez = that.rez;
+//            this->mediana = that.mediana;
+//        }
+//
+//        data& operator=(const data& that)
+//        {
+//            if (this != &that)
+//            {
+//                this->vardas = that.vardas;
+//                this->pavarde = that.pavarde;
+//                this->egz = that.egz;
+//                this->nd = that.nd;
+//                this->rez = that.rez;
+//                this->mediana = that.mediana;
+//            }
+//            return *this;
+//        }
+//
+//        ~data()
+//        {
+//            nd.clear();
+//        }
+//
+//        // rezultato sudetis
+//        data operator + (const data& b) {
+//            data rezult;
+//            rezult.rez = rez + b.rez;
+//            rezult.mediana = mediana + b.mediana;
+//            return rezult;
+//        }
+//
+//};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -49,7 +140,6 @@ class data {
 	    double rez, mediana;
     public:
         data() : vardas(""), pavarde(""), egz(0), nd(0), rez(0.0), mediana (0.0)  { }  // default konstruktorius
-
         string getVardas() const { return vardas; }
         string getPavarde() const { return pavarde; }
         int getEgzaminas() const { return egz; }
@@ -57,7 +147,6 @@ class data {
         vector<int>& getNd() { return nd; }
         double getRez() const { return rez; }
         double getMediana() const { return mediana; }
-
         void setVardas(string vardas) { this->vardas = vardas; }
         void setPavarde(string pavarde) { this->pavarde = pavarde; }
         void setEgzaminas(int egz) { this->egz = egz; }
@@ -74,7 +163,6 @@ class data {
             this->rez = that.rez;
             this->mediana = that.mediana;
         }
-
         data& operator=(const data& that)
         {
             if (this != &that)
@@ -88,13 +176,12 @@ class data {
             }
             return *this;
         }
-
         ~data()
         {
             nd.clear();
         }
 
-        // rezultatø sudëtis
+        // rezultato sudetis
         data operator + (const data& b) {
             data rezult;
             rezult.rez = rez + b.rez;
@@ -103,6 +190,23 @@ class data {
         }
 
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
